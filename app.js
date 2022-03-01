da859cec1609c152deeb6ef98c8cc29e2817d202
Johanny Adonis Lopez Mendez <adonisjalm@Apples-MacBook-Pro.local>
2022-03-01 12:53:02 -0300

    despliegue de react

--- app.js
+++ app.js
@@ -13,33 +13,33 @@ hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 // Servir contendido estatico
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('home', {
-      nombre: 'TeamPro',
-      titulo : 'NextLine'
-  } );
-});
+// app.get('/', (req, res) => {
+//   res.render('home', {
+//       nombre: 'TeamPro',
+//       titulo : 'NextLine'
+//   } );
+// });
 
-app.get('/hola-mundo', (req, res) => {
-  res.send('Hello World, en su respectiva ruta')
-});
+// app.get('/generic',  (req, res) => {
+//   res.render('generic', {
+//       nombre: 'TeamPro',
+//       titulo : 'NextLine'
+//   } );
+// })
 
-app.get('/generic',  (req, res) => {
-  res.render('generic', {
-      nombre: 'TeamPro',
-      titulo : 'NextLine'
-  } );
-})
+// app.get('/elements',  (req, res) => {
+//  res.render('elements', {
+//       nombre: 'TeamPro',
+//       titulo : 'NextLine'
+//   } );
+// })
 
-app.get('/elements',  (req, res) => {
- res.render('elements', {
-      nombre: 'TeamPro',
-      titulo : 'NextLine'
-  } );
-})
+// app.get('/hola-mundo', (req, res) => {
+//   res.send('Hello World, en su respectiva ruta')
+// });
 
-app.get('/*',  (req, res) => {
-  res.sendFile(__dirname+'/public/404.html')
+app.get('*',  (req, res) => {
+  res.sendFile(__dirname+'/public/index.html')
 })
 
 app.listen(port , () => {
